6f5d45455f9f19bdeddb109b1df1501c1f6f1890
MuhammadFarhanAkram <farhankramryk@gmail.com>
2024-10-22 13:34:25 +0500

    First commit

---
+++ AssessmentNO2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AssessmentNO2
+//
+//  Created by Muhammad  Farhan Akram on 22/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
